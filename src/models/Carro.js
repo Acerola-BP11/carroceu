0fe08b4ee70f86833ff19514080f67b3c7ff6d0c
Acerola-BP11 <felipegrego23@outlook.com>
2023-06-19 11:04:07 -0300

    fix: app start

--- src/models/Carro.js
+++ src/models/Carro.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require("sequelize")
+const sequelize = require('../sequelize')
 
 const Carro = sequelize.define('Carro', {
     id:{
--- src/sequelize.js
+++ src/sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize , DataTypes} = require('sequelize')
 
 const sequelize = new Sequelize({
     dialect:'sqlite',
-    storage:path.resolve(__dirname,'..','..','carroceu.sqlite')
+    storage:path.resolve(__dirname,'..','carroceu.sqlite')
 })
 
 module.exports = sequelize
