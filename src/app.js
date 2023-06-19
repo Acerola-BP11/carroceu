63c981a6e8a1a9f59d6c7af323378bf6c22f0280
Acerola-BP11 <felipegrego23@outlook.com>
2023-06-19 16:27:00 -0300

    Patch: Controller and Middleware

--- src/app.js
+++ src/app.js
@@ -22,19 +22,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/carros', carrosRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
